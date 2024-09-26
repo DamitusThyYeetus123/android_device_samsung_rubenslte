ed9241acec30edd2b101168728ae2bbdc253523c
DamitusThyYeetus123 <108782125+DamitusThyYeetus123@users.noreply.github.com>
2024-09-26 17:18:17 +1000

    Create system_prop.mk

---
+++ system_prop.mk
@@ -0,0 +1,16 @@
+# CABL
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.qualcomm.cabl=0
+
+# Disable RIL
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.carrier=wifi-only \
+	ro.radio.noril=1
+
+# RAM
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.config.low_ram=false
+
+# Screen
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sf.lcd_density=189
