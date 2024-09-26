930d61954f3c404cd03c958db7512b49eccec887
DamitusThyYeetus123 <108782125+DamitusThyYeetus123@users.noreply.github.com>
2024-09-26 17:29:39 +1000

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,7 +17,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from common
-$(call inherit-product, device/samsung/msm8226-common/msm8225.mk)
+$(call inherit-product, device/samsung/msm8226-common/msm8226.mk)
 
 # Inherit from vendor
 $(call inherit-product-if-exists, vendor/samsung/rubenslte/rubenslte-vendor.mk)
